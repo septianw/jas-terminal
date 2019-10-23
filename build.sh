c889115cd56e1c822eea028d3e6e8bb6796756a0
Septian Wibisono <asep.gelo@gmail.com>
2019-10-23 16:59:41 +0700

    Modify build mechanism

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+VERSION=$(cat VERSION);
+COMMIT=$(git rev-parse --short HEAD);
+
+git diff-index --quiet HEAD --
+
+if [[ $? != 0 ]]
+then
+  echo "There is uncommitted code, commit first, and build again."
+  exit 1
+fi
+
+sed "s/commitplaceholder/"$COMMIT"/g" version.template > ./package/verion.go
+sed -i "s/versionplaceholder/"$VERSION"/g" ./package/version.go
 mkdir bungkus
 go build -buildmode=plugin -ldflags="-s -w" -o bungkus/terminal.so
 cp -Rvf LICENSE CHANGELOG  module.toml schema bungkus
 mv bungkus terminal
-tar zcvvf terminal.tar.gz terminal
+tar zcvvf terminal-$VERSION-$COMMIT.tar.gz terminal
 rm -Rvf terminal
--- package/version.go
+++
@@ -1,3 +0,0 @@
-package terminal
-
-const Version = "0.1.0"
